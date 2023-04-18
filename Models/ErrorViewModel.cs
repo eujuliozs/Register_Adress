8a3f35a83660c7fced0f15a03360ce159847b09a
Julio Cesar de Souza <julios100305@gmail.com>
2023-04-18 15:39:50 -0300

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Register_with_addres.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
